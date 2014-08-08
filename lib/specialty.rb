c8bfd940754b8323770ec694cd39d650c4715141
Diane Douglas & Dustin Lewis <student@epicodus.com>
2014-08-08 12:24:15 -0700

    add save, all, and equals method to Specialty class.

--- lib/specialty.rb
+++ lib/specialty.rb
@@ -6,4 +6,23 @@ class Specialty
     @name = attributes['name']
     @id = attributes['id']
   end
+
+  def save
+    results = DB.exec("INSERT INTO specialties (name) VALUES ('#{@name}') RETURNING id;")
+    @id = results.first['id']
+  end
+
+  def self.all
+    specialties = []
+    results = DB.exec("SELECT * FROM specialties;")
+    results.each do |specialty_row|
+      current_specialty = Specialty.new(specialty_row)
+      specialties << current_specialty
+    end
+    specialties
+  end
+
+  def ==(another_specialty)
+    @name == another_specialty.name && @id == another_specialty.id
+  end
 end
--- spec/specialty_spec.rb
+++ spec/specialty_spec.rb
@@ -9,4 +9,9 @@ describe Specialty do
     expect(@test_specialty).to be_an_instance_of Specialty
     expect(@test_specialty.name).to eq "pediatrics"
   end
+
+  it 'will save to the database.' do
+    @test_specialty.save
+    expect(Specialty.all).to eq [@test_specialty]
+  end
 end
