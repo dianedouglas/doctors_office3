71639b822ae3db7614868e221e36a35e30495181
Diane Douglas & Dustin Lewis <student@epicodus.com>
2014-08-08 10:50:26 -0700

    Add save, compare and all methods to doctor class.

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -8,4 +8,29 @@ class Doctor
     @insurance_id = attributes['insurance_id'].to_i
     @id = attributes['id'].to_i
   end
+
+  def self.all
+    doctors = []
+    results = DB.exec("SELECT * FROM doctors;")
+    results.each do |result|
+      current_doc_name = result['name']
+      current_specialty_id = result['specialty_id'].to_i
+      current_insurance_id = result['insurance_id'].to_i
+      current_doc_id = result['id'].to_i
+      attributes = {'name' => current_doc_name, 'specialty_id' => current_specialty_id, 'insurance_id' => current_insurance_id, 'id' => current_doc_id}
+      current_doc = Doctor.new(attributes)
+      doctors << current_doc
+    end
+    doctors
+  end
+
+  def save
+    results = DB.exec("INSERT INTO doctors (name, specialty_id, insurance_id) VALUES ('#{@name}', '#{@specialty_id}', '#{@insurance_id}') RETURNING id;")
+    @id = results.first['id'].to_i
+  end
+
+  def ==(another_doctor)
+    (@name == another_doctor.name) && (@specialty_id == another_doctor.specialty_id) && (@insurance_id == another_doctor.insurance_id) && (@id == another_doctor.id)
+  end
+
 end
--- spec/doctor_spec.rb
+++ spec/doctor_spec.rb
@@ -15,6 +15,7 @@ end
 describe Doctor do
   before do
     @test_doctor = Doctor.new({'name' => 'Tom Baker', 'specialty_id' => 1, 'insurance_id' => 1})
+    @test_doctor2 = Doctor.new({'name' => 'Tom Baker', 'specialty_id' => 1, 'insurance_id' => 1})
   end
 
   it "will initialize a doctor class" do
@@ -25,6 +26,20 @@ describe Doctor do
     expect(@test_doctor.name).to eq ('Tom Baker')
     expect(@test_doctor.specialty_id).to eq (1)
     expect(@test_doctor.insurance_id).to eq (1)
+  end
+
+  it "should return all doctors stored in the doctor table" do
+    expect(Doctor.all).to eq []
+  end
+
+  it "should save the doctor to the doctor table" do
+    @test_doctor.save
+    expect(Doctor.all).to eq [@test_doctor]
+  end
+
+  it "should return true if two doctors have all the same attributes." do
+    @test_doctor.save
+    expect(@test_doctor==@test_doctor).to eq true
   end
 
 end
